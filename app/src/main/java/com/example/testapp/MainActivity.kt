a5d40ce1974dbae92978f4c0c32b641ee609a98b
Muhammad Faqih <freemiumlemper300@gmail.com>
2023-10-30 14:41:00 +0700

    Added Login Backend

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -13,27 +13,43 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.testapp.ui.theme.TestAppTheme
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.testapp.respond.LoginData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.security.AccessController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,16 +84,19 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting() {
+fun Greeting(navController: NavController,context: Context = LocalContext.current) {
+    val preferencesManager = remember{PreferencesManager(context = context)}
     var username by remember { mutableStateOf(TextFieldValue()) }
     var pass by remember { mutableStateOf(TextFieldValue()) }
-    var baseurl = ""
+    var baseurl = "http://10.0.2.2:1337/api/"
+    var jwt by remember { mutableStateOf("") }
+
+    jwt = preferencesManager.getData("jwt")
     val retrofit = Retrofit.builder()
         .baseUrl(baseurl)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-        .create(LoginService::class.java)
-    val call = retrofit.getData()
+        .create(LoginData::class.java)
     Column(
         modifier = Modifier
             .padding(16.dp)
@@ -100,13 +119,35 @@ fun Greeting() {
         )
 
         Spacer(modifier = Modifier.height(20.dp))
-        Button(
-            onClick = {
+        ElevatedButton(onClick = {
+            //navController.navigate("pagetwo")
+            val retrofit = Retrofit.Builder()
+                .baseUrl(baseurl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(LoginData::class.java)
+            val call = retrofit.getData(LoginData(username.text, pass.text))
+            call.enqueue(object : Callback<LoginData>{
+                override fun onResponse(call: Call<LoginRespon>, response: Response<LoginRespon>) {
+                    print(response.code())
+                    if(response.code() == 200){
+                        jwt = response.body()?.jwt!!
+                        preferencesManager.saveData("jwt", jwt)
+                        navController.navigate("pagetwo")
+                    }else if(response.code() == 400){
+                        print("error login")
+                        var toast = Toast.makeText(context, "Username atau password salah", Toast.LENGTH_SHORT).show()
 
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Login")
+                    }
+                }
+
+                override fun onFailure(call: Call<LoginData>, t: Throwable) {
+                    print(t.message)
+                }
+
+            })
+        }) {
+            Text(text = "Submit")
         }
 
     }
