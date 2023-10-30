ba4c52bcec691cb1cd2c905942d77d8ec0565ce5
Muhammad Faqih <freemiumlemper300@gmail.com>
2023-10-30 14:54:39 +0700

    Added Data

---
+++ app/src/main/java/com/example/testapp/data/DataLogin.kt
@@ -0,0 +1,5 @@
+package com.example.testapp.data
+
+import android.service.carrier.CarrierIdentifier
+
+class DataLogin(val identifier: String,val password: String)
---
+++ app/src/main/java/com/example/testapp/data/DataRegister.kt
@@ -0,0 +1,3 @@
+package com.example.testapp.data
+
+class DataRegister(val email:String,val username:String, val password:String)
